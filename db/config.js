e3456822da688949c95094e7878e115675f807a8
AnkitRajGupta21 <ankit.raj.gupta007@gmail.com>
2024-07-30 19:50:02 +0530

    change

--- db/config.js
+++ db/config.js
@@ -1,2 +1,19 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/brand_page ");
+// mongoose.connect("mongodb+srv://ankitrajgupta007:042jWdK0Dd9T01nA@cluster0.kto9ur1.mongodb.net/");
+
+// 042jWdK0Dd9T01nA
+
+// mongoose
+//     .connect("mongodb+srv://ankitrajgupta007:042jWdK0Dd9T01nA@cluster0.kto9ur1.mongodb.net/")
+//     .catch (error => console.log(error));
+
+    const connectDB = async () => {
+        try {
+          await mongoose.connect("mongodb+srv://ankitrajgupta007:042jWdK0Dd9T01nA@cluster0.kto9ur1.mongodb.net/");
+          console.log("MongoDB connected");
+        } catch (error) {
+          console.error("MongoDB connection error:", error);
+        }
+      };
+
+      connectDB()
